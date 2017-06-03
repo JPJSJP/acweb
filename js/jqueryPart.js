4514240d6427e2b538cdcb8541a56adf71a58a57
pjs <kinz7764@gmail.com>
2017-06-03 21:28:01 +0900

    'first'

---
+++ js/jqueryPart.js
@@ -0,0 +1,11 @@
+//drop-down Menu
+function jqueryPart() {
+	$(function(){
+		$('.bxslider').bxSlider({
+		  auto: true,
+		  autoControls: true
+		});
+	});
+};
+
+window.onload = function(){jqueryPart();}
---
+++ js/menuBar.js
@@ -0,0 +1,38 @@
+//drop-down Menu
+function menu() {
+	var nav = document.getElementById('navi'); 
+	var li = nav.getElementsByTagName('li'); 
+	for (var i=0; i<li.length; i++){  
+		li[i].onmouseover = function(){  
+			
+			var sub = this.getElementsByTagName('ul');
+			for (var j=0; j<sub.length; j++){
+				if (j==0){
+					sub[j].style.display = 'block';
+					sub[j].style.color = 'yellow'; 
+
+					break;
+				}
+			}
+		}
+		li[i].onmouseout = function(){
+			var sub = this.getElementsByTagName('ul');
+			for (var j=0; j<sub.length; j++){
+				if (j==0){
+					sub[j].style.display = 'none';
+					break;
+				}
+			}
+		}
+	}
+
+
+	$(function(){
+		$('.bxslider').bxSlider({
+			auto: true
+		});
+	});
+
+};
+
+window.onload = function(){menu();}
